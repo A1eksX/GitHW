e3c799d4dd44dfc88b08b2c53e9486b2e30c67bf
A1eksX <A1kon1.618@yandex.ru>
2023-11-26 22:38:17 +0300

    delete ex3() v1

--- HW1/src/main/java/Main.java
+++ HW1/src/main/java/Main.java
@@ -33,36 +33,7 @@ public class Main {
     }
 
 
-    private static void ex3() {
-//Задача 3
-//Дан массив nums = [3,2,2,3] и число val = 3.
-//Если в массиве есть числа, равные заданному, нужно перенести эти элементы в конец массива.
-//Таким образом, первые несколько (или все) элементов массива должны быть отличны от заданного, а остальные - равны ему.
-        int[] arr = {3, 2, 2, 3, 5, 6, 6, 7};
-        int val = 3;
-        int temp = 0;
-        int indexEnd = arr.length - 1;
-        int indexStart = 0;
-        for (int i = 0; indexStart < indexEnd; i++) {
-            if (arr[i] != val){
-                indexStart += 1;
-            }
-            else if (arr[i] == val) {
-                if (arr[indexEnd] != val) {
-                    temp = arr[indexEnd];
-                    arr[indexEnd] = arr[indexStart];
-                    arr[indexStart] = temp;
-                    indexStart += 1;
-                    indexEnd -= 1;
-                } else {
-                    indexEnd -= 1;
-                }
-            }
-        }
-        for (int num : arr) {
-            System.out.println(num);
-        }
-    }
+
 
 
 
